2d3f38005caa1708359d58e6725a2dcd75188ddf
sb <sb@action.ms>
2022-07-19 15:10:27 +0200

    Simulation and methods added.

--- opcua-server.py
+++ opcua-server.py
@@ -10,10 +10,15 @@ import sys
 import os
 import random
 
+from IPython import embed
+
 from opcua import Server, uamethod
 
 endpoint = "opc.tcp://127.0.0.1:4840"
 data_spec = "server.xml"
+
+# Maps node ids to digital twins.
+node_to_instance = {}
 
 
 # ### Simulation Code
@@ -24,18 +29,28 @@ class VarUpdater(Thread):
     def __init__(self, var):
         Thread.__init__(self)
         self._stopev = False
+        self._is_enabled = False
         self.var = var
 
+    def enable(self):
+        """Enable updater."""
+        self._is_enabled = True
+
+    def disable(self):
+        """Disable updater."""
+        self._is_enabled = False
+
     def stop(self):
-        """Stop updater."""
+        """Stop the updater."""
         self._stopev = True
 
     def run(self):
         """Run the thread."""
         while not self._stopev:
-            v = self.update(time.time())
-            self.var.set_value(v)
-            time.sleep(0.001)
+            if self._is_enabled:
+                v = self.update(time.time())
+                self.var.set_value(v)
+                time.sleep(0.001)
 
     def update(self, arg):
         """Updates the variable with a new value."""
@@ -49,6 +64,7 @@ class SinusUpdater(VarUpdater):
         VarUpdater.__init__(self, var)
 
     def update(self, arg):
+        print(f"SinusUpdater.update({arg})")
         return sin(arg)
 
 
@@ -59,6 +75,18 @@ class CosinusUpdater(VarUpdater):
         VarUpdater.__init__(self, var)
 
     def update(self, arg):
+        # print(f"CosinusUpdater.update({arg})")
+        return cos(arg)
+
+
+class NoiseUpdater(VarUpdater):
+    """Updates with some noise over a given value."""
+
+    def __init__(self, var, base_value: float, noise_amplitude: float):  # ### TODO implement and apply to sensor
+        VarUpdater.__init__(self, var)
+
+    def update(self, arg):
+        # print(f"NoiseUpdater.update({arg})")
         return cos(arg)
 
 
@@ -82,6 +110,7 @@ class StateGraphUpdater(VarUpdater):
         self.offset = random.randint(2, 4)
 
     def update(self, arg):
+        print(f"StateGraphUpdater.update({arg})")
         if arg > self.last_change + self.offset:
             self.stateGraph.next_state()
             self.last_change = arg
@@ -134,7 +163,8 @@ def start_mixer(parent):
     It will be exposed through the server.
     """
     print("Start the mixer.")
-    # TODO implement start and add to Mixer.
+    instance = node_to_instance[parent]
+    instance.start()
     return
 
 
@@ -145,7 +175,8 @@ def stop_mixer(parent):
     It will be exposed through the server.
     """
     print("Stop the mixer.")
-    # TODO implement stop and add to Mixer.
+    instance = node_to_instance[parent]
+    instance.stop()
     return
 
 # ### End Mixer Methods
@@ -153,30 +184,73 @@ def stop_mixer(parent):
 
 class MixerType():
     """Automated mixer Digital Twin."""
-    def __init__(self, server: Server, ns_idx: int):
+    def __init__(self, server: Server, ns_idx: int, device_id: str):
         """The mixer takes a server and namspace index required to construct an OPC-UA object type."""
         self.server = server
         self.ns_idx = ns_idx
-        self.dev = self.construct_object_type()
+        self.device_id = device_id
+        self.dev = None
+        self.state_updater = None
+        self.sensor_updater = None
+        self.is_first_call = True
+        self.construct_object_type()
 
     def construct_object_type(self):
         """Construct the OPC-UA object type for mixers."""
-        dev = server.nodes.base_object_type.add_object_type(self.ns_idx, "MixerType")
-        dev.add_variable(self.ns_idx, "sensor1", 1.0).set_modelling_rule(True)
-        dev.add_property(self.ns_idx, "device_id", "xxxx").set_modelling_rule(True)
-        ctrl = dev.add_object(self.ns_idx, "controller")
+        self.dev = server.nodes.base_object_type.add_object_type(self.ns_idx, "MixerType")
+
+        self.sensor_var = self.dev.add_variable(self.ns_idx, "sensor", 1.0)
+        self.sensor_var.set_modelling_rule(True)
+
+        self.dev.add_property(self.ns_idx, "device_id", self.device_id).set_modelling_rule(True)
+
+        ctrl = self.dev.add_object(self.ns_idx, "controller")
         ctrl.set_modelling_rule(True)
-        ctrl.add_property(self.ns_idx, "state", "Idle").set_modelling_rule(True)
-        return dev
+        self.state_var = ctrl.add_property(self.ns_idx, "state", "Idle")
+        self.state_var.set_modelling_rule(True)
+
+    def start(self):
+        """Start mixer."""
+        self.state_updater.enable()
+        self.sensor_updater.enable()
+
+    def stop(self):
+        """Stop mixer."""
+        self.state_updater.disable()
+        self.sensor_updater.disable()
+
+    def start_simulation(self):
+        """Start the updaters for simulation."""
+        if self.is_first_call:
+            print(f"Starting simulation for {self.device_id}")
+            self.state_updater.start()
+            self.sensor_updater.start()
+            self.is_first_call = False
+
+    def stop_simulation(self):
+        """Stop the updaters for simulation."""
+        self.state_updater.stop()
+        self.sensor_updater.stop()
 
 
-def create_mixer(server: Server, ns_idx: int, identifier: str):
+def create_mixer(server: Server, ns_idx: int, identifier: str) -> MixerType:
     """Create an instance of MixerType."""
-    mt = MixerType(server, ns_idx)
-    return server.nodes.objects.add_object(ns_idx, identifier, mt.dev)
+    mixer = MixerType(server, ns_idx, device_id=identifier)
+    mixer_obj = server.nodes.objects.add_object(ns_idx, identifier, mixer.dev)
+
+    state_var = mixer_obj.get_child([f"{ns_idx}:controller", f"{ns_idx}:state"])
+    mixer.state_updater = StateGraphUpdater(state_var, MixerStateGraph())
+    sensor_var = mixer_obj.get_child(f"{ns_idx}:sensor")
+    mixer.sensor_updater = SinusUpdater(sensor_var)
+
+    mixer_obj.add_method(ns_idx, "start mixer", start_mixer)
+    mixer_obj.add_method(ns_idx, "stop mixer", stop_mixer)
+    node_to_instance[mixer_obj.nodeid] = mixer
+    print(f"node id = {mixer_obj.nodeid}")
+    return mixer
+
 
 # ### End Mixer Objects
-
 
 
 
@@ -195,8 +269,15 @@ def register_namespace(server: Server) -> int:
 if __name__ == "__main__":
     server = setup_server(endpoint)
     ns_idx = register_namespace(server)
+
     mixer1 = create_mixer(server, ns_idx, "Mixer 1")
-    mx1_state_var = mixer1.get_child([f"{ns_idx}:controller", f"{ns_idx}:state"])
-    state_updater1 = StateGraphUpdater(mx1_state_var, MixerStateGraph())
     server.start()
+    server.export_xml_by_ns("opcserver.xml", [ns_idx])
+    try:
+        mixer1.start_simulation()
+        embed()
+    finally:
+        print("Cleanup simulation threads ...")
+        mixer1.stop_simulation()
+    sys.exit(0)
 
